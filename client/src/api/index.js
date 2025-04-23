efbc8997143e244bab2be2582045607cbfce598b
devayanm <devayan9689@gmail.com>
2025-04-24 02:54:30 +0530

    Added env

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { refreshAccessToken } from "./authApi";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     "Content-Type": "application/json",
   },
